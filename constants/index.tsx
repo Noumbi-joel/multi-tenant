d69b02723e4f7363ec6ef97549c653cc280328ed
Little Jacob <joelnoumbi28@gmail.com>
2023-08-21 11:00:24 +0100

    init

---
+++ constants/index.tsx
@@ -0,0 +1,320 @@
+export const SF_SORT = ["Most recent", "Most purchased"];
+
+export const FILTERS = ["All products", "Published", "Draft", "Hidden"];
+
+export const UPDATES = ["Last updated", "Name", "In stock"];
+
+export const FILTERS_CAT = ["All categories", "Published", "Draft"];
+
+export const UPDATES_CAT = ["Last updated", "Name"];
+
+export const ITEM_ACTIONS = ["Edit", "Hide", "Delete"];
+
+export const ITEM_CAT_ACTIONS = ["Edit", , "Delete"];
+
+export const ORDERS_FILTERS = ["All orders", "failed", "success", "pending"];
+
+export const ORDERS_SORT = ["Last created", "Price"];
+
+export const MODEL_BANNER =
+  "https://marketplace.canva.com/EAFTQEWI-Ko/1/0/1600w/canva-black-sky-galaxy-stars-desktop-wallpaper-iWMZ0xN6KXs.jpg";
+
+export const SF_PRICES = ["Ascendant", "Descendant"];
+
+export const OWNERS = "owners";
+
+export const STORES = "stores";
+
+export const CUSTOMERS_COLLECTION = "customers";
+
+export const CATEGORIES_COLLECTION = "categories";
+
+export const PRODUCTS_COLLECTION = "products";
+
+export const SETTINGS_COLLECTION = "settings";
+
+export const TRANSACTIONS_COLLECTION = "transactions";
+
+export const ORDERS_COLLECTION = "orders";
+
+export const SETTINGS_LEGAL = "legal";
+
+export const SETTINGS_SOCIAL_PROFILES = "socialProfiles";
+
+export const SETTINGS_GENERAL = "general";
+
+export const PENDING = "pending";
+
+export const SUCCESS = "success";
+
+export const FAILED = "failed";
+
+export const TWO_DAYS_DELIVERY = "two_days_delivery";
+
+export const EXPRESS_DELIVERY = "express_delivery";
+
+export const CUSTOMERS_SORT = [
+  "Last created",
+  "Ascending total spent",
+  "Descending total spent",
+];
+
+export const SETTINGS = [
+  { id: 1, title: "General", link: "/dashboard/settings?i=s&nav=gen" },
+  { id: 2, title: "Account", link: "/dashboard/settings?i=s&nav=acc" },
+  { id: 3, title: "Socials", link: "/dashboard/settings?i=s&nav=soc" },
+  { id: 4, title: "Legal", link: "/dashboard/settings?i=s&nav=leg" },
+];
+
+export const STORE_CATEGORIES = [
+  "Grocery",
+  "Restaurants",
+  "Hotels",
+  "Electronics",
+  "Art",
+  "furniture",
+  "Fashion",
+  "Sport",
+  "Hobbies",
+];
+
+export const VERTICAL_HEADER_ITEMS = [
+  {
+    id: 1,
+    value: "Dashboard",
+    link: "/dashboard/?index",
+  },
+  {
+    id: 2,
+    value: "Products",
+    link: "/dashboard/products/?i=p",
+  },
+  {
+    id: 3,
+    value: "Categories",
+    link: "/dashboard/categories?i=ca",
+  },
+  {
+    id: 4,
+    value: "Orders",
+    link: "/dashboard/orders?i=o",
+  },
+  {
+    id: 5,
+    value: "Customers",
+    link: "/dashboard/customers?i=cu",
+  },
+  {
+    id: 6,
+    value: "Settings",
+    link: "/dashboard/settings?i=s&nav=gen",
+  },
+];
+
+export const COUNTRY_LIST = [
+  "Afghanistan",
+  "Albania",
+  "Algeria",
+  "Andorra",
+  "Angola",
+  "Anguilla",
+  "Antigua &amp; Barbuda",
+  "Argentina",
+  "Armenia",
+  "Aruba",
+  "Australia",
+  "Austria",
+  "Azerbaijan",
+  "Bahamas",
+  "Bahrain",
+  "Bangladesh",
+  "Barbados",
+  "Belarus",
+  "Belgium",
+  "Belize",
+  "Benin",
+  "Bermuda",
+  "Bhutan",
+  "Bolivia",
+  "Bosnia &amp; Herzegovina",
+  "Botswana",
+  "Brazil",
+  "British Virgin Islands",
+  "Brunei",
+  "Bulgaria",
+  "Burkina Faso",
+  "Burundi",
+  "Cambodia",
+  "Cameroon",
+  "Cape Verde",
+  "Cayman Islands",
+  "Chad",
+  "Chile",
+  "China",
+  "Colombia",
+  "Congo",
+  "Cook Islands",
+  "Costa Rica",
+  "Cote D Ivoire",
+  "Croatia",
+  "Cruise Ship",
+  "Cuba",
+  "Cyprus",
+  "Czech Republic",
+  "Denmark",
+  "Djibouti",
+  "Dominica",
+  "Dominican Republic",
+  "Ecuador",
+  "Egypt",
+  "El Salvador",
+  "Equatorial Guinea",
+  "Estonia",
+  "Ethiopia",
+  "Falkland Islands",
+  "Faroe Islands",
+  "Fiji",
+  "Finland",
+  "France",
+  "French Polynesia",
+  "French West Indies",
+  "Gabon",
+  "Gambia",
+  "Georgia",
+  "Germany",
+  "Ghana",
+  "Gibraltar",
+  "Greece",
+  "Greenland",
+  "Grenada",
+  "Guam",
+  "Guatemala",
+  "Guernsey",
+  "Guinea",
+  "Guinea Bissau",
+  "Guyana",
+  "Haiti",
+  "Honduras",
+  "Hong Kong",
+  "Hungary",
+  "Iceland",
+  "India",
+  "Indonesia",
+  "Iran",
+  "Iraq",
+  "Ireland",
+  "Isle of Man",
+  "Israel",
+  "Italy",
+  "Jamaica",
+  "Japan",
+  "Jersey",
+  "Jordan",
+  "Kazakhstan",
+  "Kenya",
+  "Kuwait",
+  "Kyrgyz Republic",
+  "Laos",
+  "Latvia",
+  "Lebanon",
+  "Lesotho",
+  "Liberia",
+  "Libya",
+  "Liechtenstein",
+  "Lithuania",
+  "Luxembourg",
+  "Macau",
+  "Macedonia",
+  "Madagascar",
+  "Malawi",
+  "Malaysia",
+  "Maldives",
+  "Mali",
+  "Malta",
+  "Mauritania",
+  "Mauritius",
+  "Mexico",
+  "Moldova",
+  "Monaco",
+  "Mongolia",
+  "Montenegro",
+  "Montserrat",
+  "Morocco",
+  "Mozambique",
+  "Namibia",
+  "Nepal",
+  "Netherlands",
+  "Netherlands Antilles",
+  "New Caledonia",
+  "New Zealand",
+  "Nicaragua",
+  "Niger",
+  "Nigeria",
+  "Norway",
+  "Oman",
+  "Pakistan",
+  "Palestine",
+  "Panama",
+  "Papua New Guinea",
+  "Paraguay",
+  "Peru",
+  "Philippines",
+  "Poland",
+  "Portugal",
+  "Puerto Rico",
+  "Qatar",
+  "Reunion",
+  "Romania",
+  "Russia",
+  "Rwanda",
+  "Saint Pierre &amp; Miquelon",
+  "Samoa",
+  "San Marino",
+  "Satellite",
+  "Saudi Arabia",
+  "Senegal",
+  "Serbia",
+  "Seychelles",
+  "Sierra Leone",
+  "Singapore",
+  "Slovakia",
+  "Slovenia",
+  "South Africa",
+  "South Korea",
+  "Spain",
+  "Sri Lanka",
+  "St Kitts &amp; Nevis",
+  "St Lucia",
+  "St Vincent",
+  "St. Lucia",
+  "Sudan",
+  "Suriname",
+  "Swaziland",
+  "Sweden",
+  "Switzerland",
+  "Syria",
+  "Taiwan",
+  "Tajikistan",
+  "Tanzania",
+  "Thailand",
+  "Timor L'Este",
+  "Togo",
+  "Tonga",
+  "Trinidad &amp; Tobago",
+  "Tunisia",
+  "Turkey",
+  "Turkmenistan",
+  "Turks &amp; Caicos",
+  "Uganda",
+  "Ukraine",
+  "United Arab Emirates",
+  "United Kingdom",
+  "Uruguay",
+  "Uzbekistan",
+  "Venezuela",
+  "Vietnam",
+  "Virgin Islands (US)",
+  "Yemen",
+  "Zambia",
+  "Zimbabwe",
+];
---
+++ types.d.ts
@@ -0,0 +1,254 @@
+interface IProduct extends ICreateProduct {
+    visible: boolean;
+    selected: boolean;
+    isEditing: boolean;
+  }
+  
+  interface ICategory extends ICreateCategory {
+    visible: boolean;
+    selected: boolean;
+    isEditing: boolean;
+  }
+  
+  interface ICustomer {
+    id: string;
+    name: string;
+    email: string;
+    photoUrl: string;
+    role: string;
+    createdAt: string;
+    totalSpent: number;
+    totalOrders: number;
+  }
+  
+  type SignInFormProps = {
+    email: string;
+    password: string;
+    remember: boolean;
+  };
+  
+  type SignUpFormProps = {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    country: string;
+    terms: boolean;
+  };
+  
+  type StoreProps = {
+    cart: {
+      qty: number;
+      cartItems: ICart[];
+      totalQuantities: number;
+      totalPrice: number;
+      extra: string;
+    };
+    orders: {
+      orders: ICheckoutOrder[];
+      filteredOrders: ICheckoutOrder[];
+      nothingFound: boolean;
+      sortBy: string;
+      filterBy: string;
+    };
+    customers: {
+      customers: ICustomer[];
+      filteredCustomers: ICustomer[];
+      nothingFound: boolean;
+      sortBy: string;
+    };
+    products: {
+      products: IProduct[];
+      filteredProducts: IProduct[];
+      imageOne: string;
+      imageTwo: string;
+      imageThree: string;
+      imageFour: string;
+      filterProductByCategory: string;
+      filterProductByPrice: string;
+      sortProductBy: string;
+      extras: string[];
+      tags: string[];
+      nothingFound: boolean;
+      selectAllProd: false;
+      editableProduct: ICreateProduct | null;
+      isEditing: boolean;
+    };
+    profile: {
+      photoUrl: {
+        url: string;
+        name: string;
+      };
+    };
+    modal: { isGoingBack: boolean };
+    settings: {
+      store: IStore | null;
+      drawer: boolean;
+      menuDrawer: boolean;
+      cartDrawer: boolean;
+      filtersDrawer: boolean;
+      settings: {
+        legal: ILegal | null;
+        general: ISettingsGeneral | null;
+        socialProfiles: ISocialProfile | null;
+      };
+      favicon: {
+        url: string;
+        name: string;
+      };
+      logo: { url: string; name: string };
+      bannerImage: { url: string; name: string };
+    };
+    categories: {
+      categories: ICategory[];
+      image: { src: string; name: string };
+      filteredCategories: ICategory[];
+      nothingFound: boolean;
+      selectAllCat: false;
+      editableCategory: ICreateCategory;
+      isEditing: boolean;
+    };
+  };
+  
+  type CreateStoreProps = {
+    storeName: string;
+    storeCategory: string;
+  };
+  
+  interface IStore {
+    id: string;
+    createdAt: Date;
+    storeCategory: string;
+    storeName: string;
+    owner: {
+      ownerEmail: string;
+      ownerId: number;
+      ownerName: string;
+      ownerPhotoUrl: string;
+    };
+  }
+  
+  interface ICreateProduct {
+    id: string;
+    name: string;
+    description: string;
+    price: string;
+    salePrice: string;
+    status: string;
+    category: string;
+    inventory: string;
+    isFragile: boolean;
+    createdAt: string;
+    updatedAt: string;
+    imageOne: string;
+    imageTwo: string;
+    imageThree: string;
+    imageFour: string;
+    tags: string[];
+    extras: string[]
+    purchaseNumber: number;
+  }
+  
+  interface ICreateCategory {
+    id: string;
+    name: string;
+    description: string;
+    status: string;
+    image: string;
+    createdAt: string;
+    items: string;
+    updatedAt: string;
+  }
+  
+  interface ILegal {
+    id: string;
+    refundPolicy: string;
+    privacyPolicy: string;
+    shippingPolicy: string;
+  }
+  
+  interface ISocialProfile {
+    id: string;
+    facebook: string;
+    instagram: string;
+    twitter: string;
+    tiktok: string;
+  }
+  
+  interface IAdminAccount {
+    imageUrl: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+  }
+  
+  interface ISettingsGeneral {
+    id: string;
+    storeName: string;
+    storeDescription: string;
+    city: string;
+    postalCode: string;
+    country: string;
+    state: string;
+    bannerImage: string;
+    faviconImage: string;
+    storeLogo: string;
+    bannerMainTitle: string;
+    bannerDetails: string;
+    mainTitle: string;
+    mainTitleDetails: string;
+    mainTitleCategory: string;
+    mainTitleCategoryDetails: string;
+  }
+  
+  interface ICart {
+    id: string;
+    image: string;
+    price: number;
+    extras: string[];
+    quantity: number;
+    name: string;
+  }
+  
+  interface ICheckout {
+    fullName: string;
+    phone: string;
+    fullAddress: string;
+    email: string;
+    country: string;
+    expressDelivery: boolean;
+    twoDaysDelivery: boolean;
+    saveNextTime: boolean;
+  }
+  
+  interface ITransaction {
+    id: string;
+    orderId: string;
+    method: string;
+    status: string;
+    amount: string;
+    createdAt: string;
+    updatedAt: string;
+  }
+  
+  interface IOrder {
+    id: string;
+    uid: string;
+    status: string;
+    deliveryMethod: string;
+    products: ICart[];
+    subtotal: string;
+    cost: string;
+    totalPrice: string;
+    totalQuantities: string;
+    contactDetails: {
+      fullName: string;
+      phone: string;
+      fullAddress: string;
+      email: string;
+      country: string;
+    };
+    createdAt: string;
+    updatedAt: string;
+  }
+  

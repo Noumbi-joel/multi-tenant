76ab485e5b8a10c9f3bf6c0c033b197de8c2e4c2
Little Jacob <joelnoumbi28@gmail.com>
2023-09-07 11:43:22 +0100

    added pages

---
+++ pages/_sites/site/contact.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StoreFrontContactPage() {
+  return (
+    <div>StoreFrontContactPage</div>
+  )
+}
---
+++ pages/_sites/site/refund-policy.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StoreFrontRefundPolicyPage() {
+  return (
+    <div>StoreFrontRefundPolicyPage</div>
+  )
+}

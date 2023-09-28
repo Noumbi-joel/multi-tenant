639f03c76bab846125614c97703cd62880a11e48
Little Jacob <joelnoumbi28@gmail.com>
2023-09-28 13:03:42 +0100

    updates

--- components/admin/AdminLayout.tsx
+++ components/admin/AdminLayout.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { VerticalNav, Drawer, ErrorOverlay } from "@/components";
 import { ErrorBoundary } from "react-error-boundary";
+import { RxCross1, RxPlus } from "react-icons/rx";
 
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-const [isVisible, setIsVisible] = React.useState(false);
+  const [isVisible, setIsVisible] = React.useState(false);
   return (
     <div className="flex bg-white">
       <ErrorBoundary FallbackComponent={ErrorOverlay}>
@@ -17,9 +18,20 @@ const [isVisible, setIsVisible] = React.useState(false);
           onClose={() => setIsVisible(!isVisible)}
           className="w-fit"
         >
-          <VerticalNav drawer={isVisible} />
+          <VerticalNav />
         </Drawer>
-        
+
+        <button
+          onClick={() => setIsVisible(!isVisible)}
+          className="md:hidden flex z-99 fixed bottom-5 right-5 w-9 h-9 items-center justify-center bg-primary-base rounded-full"
+        >
+          {!isVisible ? (
+            <RxPlus className="w-5 h-5 text-white" />
+          ) : (
+            <RxCross1 className="w-5 h-5 text-white" />
+          )}
+        </button>
+
         {children}
       </ErrorBoundary>
     </div>
